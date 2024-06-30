1e60188f71e26f0d15852cbea1193646924012bb
Yash <57257390+dicusa@users.noreply.github.com>
2024-06-30 18:02:08 +0530

    update_readme

--- update_readme.py
+++ update_readme.py
@@ -27,7 +27,7 @@ def update_readme(posts):
         end_index = readme_content.index("<!-- BLOG-POST-THUMBNAILS:END -->\n")
 
         new_content = ["<!-- BLOG-POST-THUMBNAILS:START -->\n"]
-        new_content.append('<div style="display:flex; overflow: auto; white-space: nowrap; padding: 10px; gap: 10px;">\n')
+        new_content.append('<div style="white-space: nowrap; overflow: auto; padding: 10px; gap: 10px;">\n')
 
         for post in posts:
             new_content.append(
